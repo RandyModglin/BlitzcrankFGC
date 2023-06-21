e4ad8a2b81b397b2cebb59fe7b09f90fa37acb92
RandyModglin <94387047+RandyModglin@users.noreply.github.com>
2023-06-20 22:50:08 -0400

    Hitbox Blueprint and Materials

---
+++ Source/BlitzcrankFGC/HitboxActor.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HitboxActor.generated.h"
+
+
+UENUM(BlueprintType)
+enum class HitboxEnum : uint8
+{
+	HB_Proximity UMETA(DisplayName = "Proximity"),
+	HB_Strike UMETA(DisplayName = "Strike"),
+	HB_Hurtbox UMETA(DisplayName = "Hurtbox")
+};
+
+UCLASS()
+class BLITZCRANKFGC_API AHitboxActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHitboxActor();
+
+
+	//Hitbox Enum Type
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hitbox")
+		HitboxEnum hitboxType;
+
+	//Location to spawn the hitbox
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hitbox")
+		FVector hitboxLocation;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
